bfaada801b57de35e2e19ce9c7f29302bb24e362
shahinur rahman <cstshahinur17@gmail.com>
2024-12-28 02:39:13 +0600

    added the food card section

--- src/Pages/Foods.jsx
+++ src/Pages/Foods.jsx
@@ -1,5 +1,11 @@
+import FoodsCard from "../component/ProductsCard/FoodsCard";
+import data from "../data/product.json";
 const Foods = () => {
-  return <div>this is food pagess</div>;
+  return (
+    <div>
+      <FoodsCard data={data} />
+    </div>
+  );
 };
 
 export default Foods;
